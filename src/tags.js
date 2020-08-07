b718d23ad305048ed7cac1cb790805aed5948b97
Karl Holub <holub008@umn.edu>
2020-08-06 20:07:06 -0500

    functional hierarchy

--- src/tags.js
+++ src/tags.js
@@ -38,24 +38,104 @@ function recurseListCombinations(data) {
   return combinations;
 }
 
+/**
+ * note, the collector is mutated (in addition to being returned)
+ */
+function bfsCollect(root, adjacency, collector=new Set()) {
+  adjacency[root].forEach(ix => {
+    if (!(ix in collector)) {
+      collector.add(ix);
+      bfsCollect(ix, adjacency, collector);
+    }
+  });
+
+  return collector;
+}
+
 class TagHierarchy {
-  constructor(adjacency, orderedTagNames) {
+
+  static createFromEdgeList(edgeList, tags) {
+    const tagIdToIndex = {};
+    const tagNameToIndices = {};
+    tags.forEach((t, ix) => {
+      tagIdToIndex[t.id] = ix;
+      if (!tagNameToIndices[t.name]) {
+        tagNameToIndices[t.name] = [ix];
+      }
+      else {
+        tagNameToIndices.push(ix);
+      }
+    });
+
+    const adjacency = Array.from(Array(tags.length), () => []);
+    edgeList.forEach(e => {
+      const fromIx = tagIdToIndex[e.from];
+      const toIx = tagIdToIndex[e.to];
+      const fromArr = adjacency[fromIx];
+      fromArr.push(toIx);
+    });
+
+    return new TagHierarchy(adjacency, tagNameToIndices);
+  }
+
+  /**
+   * @param adjacency an array of array of indices, representing a sparse graph adjacency matrix
+   * @param tagNameToIndices an object with key tag names and value index into the adjacency matrix
+   */
+  constructor(adjacency, tagNameToIndices) {
     this._adjacency = adjacency;
-    this._tagNames = orderedTagNames;
-    this._tagNameToIndices =buildInvertedIndex(orderedTagNames);
+    this._tagNameToIndices = tagNameToIndices;
   }
 
   containsTag(name) {
     return this._tagNameToIndices[name] !== undefined;
   }
 
-  getIndices(namePath) {
-    const allIds = namePath.map(n => this._tagNameToIndices[n]);
+  getIndices(tagName) {
+    const indices = this._tagNameToIndices[tagName];
+    if (!indices) {
+      return [];
+    }
 
+    return indices;
+  }
+
+  /**
+   *
+   * @param indexPath an array of arrays containing tag indices representing potential paths
+   * @return an array of indices representing tags at the end of the path
+   */
+  getIndicesForPath(indexPath) {
+    const candidatePaths = recurseListCombinations(indexPath);
     const validPaths = [];
+    candidatePaths.forEach(p => {
+      let exists = true;
+      for (let ix = 0; ix < (p.length - 1); ix++) {
+        const fromIx = p[ix];
+        const toIx = p[ix + 1];
+        exists = toIx in this._adjacency[fromIx];
+      }
 
+      if (exists) {
+        validPaths.push(p);
+      }
+    });
 
+    return validPaths;
+  }
+
+  explode(indices) {
+    // perform a BFS from the indices - note, the hierarchy should be a tree/forest, but still use a cycle-safe
+    // implementation. we can safely share the collector, to the benefit of not repeating work
+    const collector = new Set();
+    indices.forEach(root => {
+      bfsCollect(root, this._adjacency, collector);
+    });
+
+    return [...collector]
   }
 }
 
-module.exports
+module.exports = {
+  TagHierarchy,
+};
--- test/test_tags.js
+++ test/test_tags.js
@@ -1,11 +1,42 @@
+const tg = require('../src/tags');
+
 describe('tag hierarchy', function () {
+  const edges = [
+    {
+      from: 1,
+      to: 2,
+    },
+    {
+      from: 1,
+      to: 3,
+    },
+    {
+      from: 2,
+      to: 4
+    }
+  ];
 
+  const tags = [
+    {
+      id: 1,
+      name: 'tag1',
+    },
+    {
+      id: 2,
+      name: 'tag2',
+    },
+    {
+      id: 3,
+      name: 'tag3',
+    },
+    {
+      id: 4,
+      name: 'tag4'
+    },
+  ];
 
-  it('should produce a depth 1 AST', function () {
-    const target = new ast.Node(tkSequence[1], ops.Operators.and);
-    target.addChild(new ast.Node(tkSequence[0]));
-    target.addChild(new ast.Node(tkSequence[2]));
-
-    assert.ok(ast.nodesEqual(parser.parse(tkSequence), target, true));
+  it('can be constructed correctly for a depth 2 tree', function () {
+    const hierarchy = tg.TagHierarchy.createFromEdgeList(edges, tags);
+    //assert.equal(hierarchy.);
   });
 });
